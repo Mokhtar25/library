998da5db4d2118d20329efadc848683204136800
mohtar25 <mokhtar2000naj@gmail.com>
2024-03-28 15:27:47 +0100

    done with v1

--- library.js
+++ library.js
@@ -16,8 +16,8 @@ function Book(title, author, pages, read){
         
     }
 }
-
-const albert = new Book(`the stranger`, `albert camus`, 100, 1);
+ 
+const albert = new Book(`the stranger`, `albert camus`, 100, false);
 
 mylibrary.push(albert)
 
@@ -32,14 +32,48 @@ addbook("Normal People", "Salley Ronney", 150, false);
 addbook("Maybe You should talk to someone", "Lori Gottlieb", 300, true);
 addbook("The Song of Achilles", "Madline Miller", 250, true)
 
+const cards = document.querySelector('.main');
+
+
 function display(){
+    const frag  = document.createDocumentFragment();
+    cards.replaceChildren();
+
 for (book of mylibrary){
-    // console.log(book.info());
+    let container = document.createElement('div');
+    container.classList.add('card');
+    let title_div = document.createElement('div');
+    title_div.innerText = book.title;
+    title_div.classList.add('card_title');
+    container.appendChild(title_div);
+    let author_div = document.createElement('div');
+    author_div.classList.add('card_author');
+    author_div.innerText = book.author;
+    container.appendChild(author_div);
+    let pages_div = document.createElement('div');
+    pages_div.classList.add('card_pages');
+    pages_div.innerText = book.pages;
+    container.appendChild(pages_div);
+    let checked_div = document.createElement('div');
+    checked_div.classList.add('card_checked');
+    checked_div.innerText = 'Read'
+    if (book.read === false){
+        checked_div.classList.add('card_checked_not');
+    }
+    container.appendChild(checked_div);
+    let close_btn = document.createElement('div');
+    close_btn.classList.add('close-button','close', 'card_close');
+    close_btn.innerText = 'X'
+    container.appendChild(close_btn);
+
+    frag.appendChild(container);
 }
+cards.appendChild(frag);
+remove();
 }
 const dialog = document.querySelector('#dialog')
 const open_button = document.querySelector('.open-button');
-const close_button = document.querySelector('.close-button');
+const close_button = document.querySelector('#submit_form');
 
 open_button.addEventListener("click", ()=>{
     dialog.showModal();
@@ -56,7 +90,7 @@ const title_input = document.querySelector('.title');
 const author_input = document.querySelector('.author');
 const pages_input = document.querySelector('.pages');
 const read_input = document.querySelector('.read');
-const close_header = document.querySelector('.close');
+const close_header = document.querySelector('#close_dialog');
 
 close_header.addEventListener("click", ()=>{
     dialog.close();
@@ -64,20 +98,66 @@ close_header.addEventListener("click", ()=>{
     author_input.value = '';
     pages_input.value = '';
     read_input.checked = false;
+
 })
+
+
 
 const form = document.querySelector('form');
 close_button.addEventListener("click", ()=>{
 
+    if (form.checkValidity()){
     title = title_input.value;
     author = author_input.value;
     pages = pages_input.value;
     if (read_input.checked){
         read = true
     }
-
+    
     addbook(title, author, pages, read);
     display();
+    form.reset()    
+    dialog.close();
+    } 
 })
+display();
 
 
+function remove(){
+const cards_ = document.querySelectorAll('.main .card')
+ 
+cards_.forEach((e)=>{
+        let data =  e.querySelector('.close');
+    data.addEventListener('click', ()=>{
+        console.log('clicked', e.textContent)
+        let title   = e.firstChild.textContent
+        console.log(title);
+         let index = mylibrary.findIndex((elm)=> elm.title == title)
+         console.log(index);
+        if (index !== -1){
+            mylibrary.splice(index, 1);
+        }
+        display();
+     })
+    let read_btn = e.querySelector('.card_checked');
+    read_btn.addEventListener('click', ()=>{
+        let book = e.firstChild.textContent;
+        console.log("click")
+        let index_r = mylibrary.findIndex((el) => el.title == book);
+        if (index_r !== -1){
+            if (mylibrary[index_r].read === true){
+                mylibrary[index_r].read = false;
+                e.classList.add('card_checked_not');
+            }
+            else{
+                mylibrary[index_r].read = true;
+                e.classList.remove('card_checked_not');
+            }
+        }
+        display();
+    })
+
+
+    
+})
+}
